11de84fc25537255ae917de33340ef5ada44c4dc
lanoliveira <alan_a.oliveira@outlook.com>
2021-11-08 00:48:19 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,119 @@
+var blocoLogin = document.querySelector('.bloco-login'),
+bloco = document.querySelector('.bloco'),
+blocoI = document.querySelector('.bloco-i'),
+btLogin = document.querySelectorAll('.bt-sign-in'),
+btUpload = document.querySelector('#Sign-up'),
+btSign = document.querySelector('.ul-2-middle'),
+avatarShow = document.querySelector('#avatar'),
+avatar = document.querySelector('#avatar-img'),
+searchIcon = document.querySelector('#search'),
+search = document.querySelector('#show-search'),
+formSearch = document.querySelector('#form-search'),
+searchImput = document.querySelector('#search-imput'),
+ol = document.querySelector('.principal'),
+form = document.querySelector('#form');
+
+request = new XMLHttpRequest();
+
+
+if (localStorage.length > 0){
+signIn();
+}
+
+btLogin.forEach(bt => bt.addEventListener('click', function () {
+bloco.className += ' bloco-log';
+blocoI.className += ' none';
+blocoLogin.className = 'bloco-login';
+}));
+
+searchIcon.addEventListener('click', function () {
+search.className = 'show-search';
+bloco.className = 'none'
+});
+
+function btSubmit() {
+data = {email: form.elements['email'].value, password: form.elements['password'].value};
+//data = { email: "eve.holt@reqres.in", password: "cityslicka" };
+postData(data);
+};
+
+formSearch.addEventListener('submit', (ev)=>{
+    ev.preventDefault();
+});
+
+searchImput.addEventListener('keyup', function (e) {
+if (e.keyCode === 13) {
+    getBusca();
+}
+});
+
+
+
+async function getBusca() {
+    let json = await axios.get('https://pixabay.com/api/?key=24231893-90c8178f8275c01f0c83fc75b&q='+ searchImput.value +'&image_type=photo' + '/json');
+    console.log(json.data);
+
+    if(json.data.total != 0){
+        for (var i = 0; i < 8; i++) {
+
+            div = document.createElement('div');
+            a = document.createElement('a');
+            img = document.createElement('img');
+            li = document.createElement('li');
+
+            div.className = 'main-img';
+            img.widht = "330";
+            img.height = "247";
+            img.src = json.data.hits[i].webformatURL;
+            a.appendChild(img);
+            div.appendChild(a);
+            li.appendChild(div);
+            ol.insertBefore(li, ol.firstChild);
+        }
+    } 
+};
+
+async function getData() {
+
+let json = await axios.get('https://reqres.in/api/users?page=1' + '/json');
+console.log(json.data.data);
+
+for (var i = 0; i < json.data.data.length; i++) {
+    if (json.data.data[i].email == data.email) {
+        localStorage.setItem('user', JSON.stringify(json.data.data[i]));
+    }
+}
+signIn();
+};
+
+
+function postData(data) {
+
+request.open('POST', 'https://reqres.in/api/login', true);
+
+request.responseType = 'json';
+request.setRequestHeader('Content-Type', 'application/json');
+
+request.onload = function () {
+    if (request.status > 200) {
+        // criar o tratamento de erro
+        console.log(request.response);
+    } else {
+        // console.log(request.response);
+        getData(data);
+    }
+};
+request.send(JSON.stringify(data));
+};
+
+function signIn() {
+bloco.className = 'none';
+blocoI.className = 'bloco-i';
+blocoI.className = 'none';
+blocoLogin.className = 'none';
+btUpload.innerHTML = 'Upload';
+btSign.style.display = 'none';
+avatarShow.removeAttribute('style');
+avatar.setAttribute('src', JSON.parse(localStorage.getItem('user')).avatar);
+searchIcon.removeAttribute('style');
+}
