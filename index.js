e794068be2d176733cb531d1a244fb8c01fde95d
lanoliveira <alan_a.oliveira@outlook.com>
2021-11-11 16:33:10 -0300

    second commit

--- index.js
+++ index.js
@@ -1,61 +1,69 @@
 var blocoLogin = document.querySelector('.bloco-login'),
-bloco = document.querySelector('.bloco'),
-blocoI = document.querySelector('.bloco-i'),
-btLogin = document.querySelectorAll('.bt-sign-in'),
-btUpload = document.querySelector('#Sign-up'),
-btSign = document.querySelector('.ul-2-middle'),
-avatarShow = document.querySelector('#avatar'),
-avatar = document.querySelector('#avatar-img'),
-searchIcon = document.querySelector('#search'),
-search = document.querySelector('#show-search'),
-formSearch = document.querySelector('#form-search'),
-searchImput = document.querySelector('#search-imput'),
-ol = document.querySelector('.principal'),
-form = document.querySelector('#form');
+    bloco = document.querySelector('.bloco'),
+    blocoI = document.querySelector('.bloco-i'),
+    btLogin = document.querySelectorAll('.bt-sign-in'),
+    btUpload = document.querySelector('#Sign-up'),
+    btSign = document.querySelector('.ul-2-middle'),
+    avatarShow = document.querySelector('#avatar'),
+    avatar = document.querySelector('#avatar-img'),
+    searchIcon = document.querySelector('#search'),
+    search = document.querySelector('#show-search'),
+    formSearch = document.querySelector('#form-search'),
+    searchImput = document.querySelector('#search-imput'),
+    ol = document.querySelector('.principal'),
+    dialog = document.querySelector('dialog'),
+    backDialog = document.getElementById('back'),
+    getError = document.getElementById('getError'),
+    form = document.querySelector('#form');
 
 request = new XMLHttpRequest();
 
 
-if (localStorage.length > 0){
-signIn();
+if (localStorage.token != undefined) {
+    signIn();
 }
 
 btLogin.forEach(bt => bt.addEventListener('click', function () {
-bloco.className += ' bloco-log';
-blocoI.className += ' none';
-blocoLogin.className = 'bloco-login';
+    bloco.className += ' bloco-log';
+    blocoI.className += ' none';
+    blocoLogin.className = 'bloco-login';
 }));
 
 searchIcon.addEventListener('click', function () {
-search.className = 'show-search';
-bloco.className = 'none'
+    search.className = 'show-search';
+    bloco.className = 'none';
 });
 
 function btSubmit() {
-data = {email: form.elements['email'].value, password: form.elements['password'].value};
-//data = { email: "eve.holt@reqres.in", password: "cityslicka" };
-postData(data);
+    data = { email: form.elements['email'].value, password: form.elements['password'].value };
+    //data = { email: "eve.holt@reqres.in", password: "cityslicka" };
+    postData(data);
 };
 
-formSearch.addEventListener('submit', (ev)=>{
+function btCloseDialog() {
+    dialog.close();
+    backDialog.removeAttribute('class');
+    searchImput.value = '';
+}
+
+formSearch.addEventListener('submit', (ev) => {
     ev.preventDefault();
 });
 
 searchImput.addEventListener('keyup', function (e) {
-if (e.keyCode === 13) {
-    getBusca();
-}
+    if (e.keyCode === 13) {
+        getBusca();
+    }
 });
 
 
 
 async function getBusca() {
-    let json = await axios.get('https://pixabay.com/api/?key=24231893-90c8178f8275c01f0c83fc75b&q='+ searchImput.value +'&image_type=photo' + '/json');
+    let json = await axios.get('https://pixabay.com/api/?key=24231893-90c8178f8275c01f0c83fc75b&q=' + searchImput.value + '&image_type=photo' + '/json');
     console.log(json.data);
 
-    if(json.data.total != 0){
+    if (json.data.total != 0) {
         for (var i = 0; i < 8; i++) {
-
             div = document.createElement('div');
             a = document.createElement('a');
             img = document.createElement('img');
@@ -70,50 +78,60 @@ async function getBusca() {
             li.appendChild(div);
             ol.insertBefore(li, ol.firstChild);
         }
-    } 
+    } else {
+        backDialog.className = 'backDialog';
+        getError.innerHTML = 'No data found';
+        dialog.show();
+    }
 };
 
 async function getData() {
 
-let json = await axios.get('https://reqres.in/api/users?page=1' + '/json');
-console.log(json.data.data);
+    let json = await axios.get('https://reqres.in/api/users?page=1' + '/json');
+    console.log(json.data.data);
 
-for (var i = 0; i < json.data.data.length; i++) {
-    if (json.data.data[i].email == data.email) {
-        localStorage.setItem('user', JSON.stringify(json.data.data[i]));
+    for (var i = 0; i < json.data.data.length; i++) {
+        if (json.data.data[i].email == data.email) {
+            localStorage.setItem('user', JSON.stringify(json.data.data[i]));
+        }
     }
-}
-signIn();
+    signIn();
 };
 
 
 function postData(data) {
 
-request.open('POST', 'https://reqres.in/api/login', true);
+    request.open('POST', 'https://reqres.in/api/login', true);
 
-request.responseType = 'json';
-request.setRequestHeader('Content-Type', 'application/json');
+    request.responseType = 'json';
+    request.setRequestHeader('Content-Type', 'application/json');
 
-request.onload = function () {
-    if (request.status > 200) {
-        // criar o tratamento de erro
-        console.log(request.response);
-    } else {
-        // console.log(request.response);
-        getData(data);
-    }
-};
-request.send(JSON.stringify(data));
+    request.onload = function () {
+        if (request.status >= 400) {
+            backDialog.className = 'backDialog';
+            getError.innerHTML = request.response.error;
+            dialog.show();
+            if (request.response.error === 'user not found') {
+                form.elements['email'].value = '';
+                form.elements['password'].value = '';
+            }
+        } else {
+            console.log(request.response);
+            localStorage.setItem('token', request.response.token);
+            getData(data);
+        }
+    };
+    request.send(JSON.stringify(data));
 };
 
 function signIn() {
-bloco.className = 'none';
-blocoI.className = 'bloco-i';
-blocoI.className = 'none';
-blocoLogin.className = 'none';
-btUpload.innerHTML = 'Upload';
-btSign.style.display = 'none';
-avatarShow.removeAttribute('style');
-avatar.setAttribute('src', JSON.parse(localStorage.getItem('user')).avatar);
-searchIcon.removeAttribute('style');
+    bloco.className = 'none';
+    blocoI.className = 'bloco-i';
+    blocoI.className = 'none';
+    blocoLogin.className = 'none';
+    btUpload.innerHTML = 'Upload';
+    btSign.style.display = 'none';
+    avatarShow.removeAttribute('style');
+    avatar.setAttribute('src', JSON.parse(localStorage.getItem('user')).avatar);
+    searchIcon.removeAttribute('style');
 }
